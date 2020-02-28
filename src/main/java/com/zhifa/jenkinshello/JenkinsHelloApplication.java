156a81cdcbc7e15bbc770a2970e205fcd4fb2d81
xiaozhi止水 <877657682@qq.com>
2020-02-28 22:37:57 +0800

    简单的接口

--- src/main/java/com/zhifa/jenkinshello/JenkinsHelloApplication.java
+++ src/main/java/com/zhifa/jenkinshello/JenkinsHelloApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsHelloApplication {
 
+    /**
+     * docker build -t runoob/ubuntu:v1 .
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(JenkinsHelloApplication.class, args);
     }
